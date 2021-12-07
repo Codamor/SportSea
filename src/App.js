02f981f86d16f9bad304c45a045d74192e7fa57e
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-12-07 22:48:16 +0100

    refator Container className : --vertical > --horizontal

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component{
         return(
             <BrowserRouter>
                 <Header />
-                <Container className="container--vertical">
+                <Container className="container--horizontal">
                     <Sidebar />
                     <Main />
                 </Container>
