f32bc6b99a1fb68c561bd532266e80967756e619
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-12-07 15:08:50 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <div>
-      <h1>SportSee</h1>
-    </div>
-  );
+import React from "react";
+import {BrowserRouter} from "react-router-dom";
+
+
+class App extends React.Component{
+
+    render(){
+        return(
+            <BrowserRouter>
+                <h1>SportSee</h1>
+            </BrowserRouter>
+
+        )
+    }
 }
 
-export default App;
+export default App ;
