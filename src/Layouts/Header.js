045b35b235eed21580d4a6c1fe5c05e1e0925c59
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-12-07 16:31:07 +0100

    add basic styles to Header Component

--- src/Layouts/Header.js
+++ src/Layouts/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Logo from "../Components/Logo/Logo";
+import Navigation from "../Components/Navigation/Navigation";
+
+import "./Header.scss"
 
 class Header extends React.Component{
     render() {
         return(
             <header className="header">
                 <Logo />
+                <Navigation />
             </header>
         )
     }
