0a89b3c05976ab1b13ca5aec17e86046b5c3f130
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-12-07 22:22:05 +0100

    add Sidebar Component

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Sidebar.scss";
+
+import meditationLogo from "../../assets/img/meditation.png";
+import swimmingLogo from "../../assets/img/swimming.png";
+import bikingLogo from "../../assets/img/biking.png";
+import weightsLogo from "../../assets/img/weights.png";
+
+class Sidebar extends React.Component{
+    render() {
+        return (
+            <aside className="sidebar">
+                <ul className="sidebar__list">
+                    <li className="sidebar__item">
+                        <a className="sidebar__link" href="/#">
+                            <img src={meditationLogo} alt="Meditation" title="Meditation"/>
+                        </a>
+                    </li>
+                    <li className="sidebar__item">
+                        <a className="sidebar__link" href="/#">
+                            <img src={swimmingLogo}  alt="Natation" title="Natation"/>
+                        </a>
+                    </li>
+                    <li className="sidebar__item">
+                        <a className="sidebar__link" href="/#">
+                            <img src={bikingLogo}  alt="Cyclisme" title="Cyclisme"/>
+                        </a>
+                    </li>
+                    <li className="sidebar__item">
+                        <a className="sidebar__link" href="/#">
+                            <img src={weightsLogo}  alt="Altères" title="Altères"/>
+                        </a>
+                    </li>
+                </ul>
+            </aside>
+        );
+    }
+}
+
+export default Sidebar ;
