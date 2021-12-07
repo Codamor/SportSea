a8ac9b737cbc8aa0865c0e6d641d8e79219a53f8
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-12-07 22:40:18 +0100

    add link to Logo

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,11 +1,13 @@
 import React from "react";
+import {Link} from "react-router-dom";
+
 import logo from "../../assets/img/logo.png";
 import "./Logo.scss" ;
 
 class Logo extends React.Component{
     render() {
         return (
-            <div className="logo">
+            <Link to="/" className="logo">
                 <img src={logo}
                      alt="Suivez votre activité quotidienne avec Sportsee"
                      title="Suivez votre activité quotidienne avec Sportsee"/>
@@ -13,7 +15,7 @@ class Logo extends React.Component{
                 <h1 className="logo__title">
                     SportSee
                 </h1>
-            </div>
+            </Link>
         );
     }
 }
