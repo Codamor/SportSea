9ca5e987f86cb21f8495b29705bb237a8ddd1ff4
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-12-15 16:43:53 +0100

    add children to Main

--- src/Layouts/Main/Main.js
+++ src/Layouts/Main/Main.js
@@ -2,10 +2,16 @@ import React from "react";
 import "./Main.scss" ;
 
 class Main extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
     render() {
         return (
             <main className="main">
-
+                {
+                    this.props.children
+                }
             </main>
         );
     }
