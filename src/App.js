b1d5566cd6d8253d29ea5e756c60b4e69dcb94d1
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-12-16 14:48:46 +0100

    move Sidebar component from Components to Layouts

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import {BrowserRouter} from "react-router-dom";
 import Header from "./Layouts/Header/Header";
 import Container from "./Components/Container/Container";
-import Sidebar from "./Components/Sidebar/Sidebar";
+import Sidebar from "./Layouts/Sidebar/Sidebar";
 import Main from "./Layouts/Main/Main";
+import KeyData from "./Components/KeyData/KeyData";
 
 class App extends React.Component{
 
@@ -15,7 +16,7 @@ class App extends React.Component{
                 <Container className="container--horizontal">
                     <Sidebar />
                     <Main>
-
+                        <KeyData />
                     </Main>
                 </Container>
             </BrowserRouter>
