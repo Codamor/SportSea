1720ce82da7e6e6d6a3f893dcd2579f22e253223
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-12-17 16:12:20 +0100

    add UserPage basic component structure

---
+++ src/Layouts/UserPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class UserPage extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <main>
+
+            </main>
+        );
+    }
+
+}
+
+export default UserPage ;
