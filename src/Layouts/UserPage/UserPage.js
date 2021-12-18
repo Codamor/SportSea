3065cdeb5827d9820539d2c8e8604e3f0c7bb81c
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-12-18 14:49:21 +0100

    get userInfos from api

--- src/Layouts/UserPage/UserPage.js
+++ src/Layouts/UserPage/UserPage.js
@@ -1,19 +1,44 @@
 import React from "react";
+import Container from "../../Components/Container/Container";
+import Main from "../../Components/Main/Main";
+import Informations from "../../Components/Informations/Informations";
+import {getUserActivity, getUserInformations} from "../../adapters/api";
 
 class UserPage extends React.Component{
     constructor(props) {
         super(props);
         this.userId = this.props.match.params.id;
+        this.state = {
+            userName: ""
+        }
     }
 
     render() {
 
 
         return (
-            <main>
+            <Main>
 
-            </main>
+                <Container className="container--vertical">
+
+                </Container>
+
+            </Main>
         );
+    }
+
+    componentDidMount() {
+
+        getUserInformations(this.userId)
+            .then(data => {
+                this.setState(
+                    {
+                        userName: data.userInfos.firstName
+                    }
+                )
+            })
+
+
     }
 
 }
