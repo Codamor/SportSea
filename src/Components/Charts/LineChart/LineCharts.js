b00393e8c2562bed35f6ef9d34a2959201afefc7
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-12-20 14:41:48 +0100

    add LineChart css class

--- src/Components/Charts/LineChart/LineCharts.js
+++ src/Components/Charts/LineChart/LineCharts.js
@@ -1,10 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import {
+    LineChart,
+    Line,
+    XAxis,
+    Tooltip,
+    ResponsiveContainer
+} from 'recharts';
 
 
 import "./LineCharts.scss";
+
 
 class LineCharts extends React.Component{
     constructor(props) {
@@ -13,28 +20,51 @@ class LineCharts extends React.Component{
     }
 
     render() {
+
+        const CustomTooltip = ({ active, payload}) => {
+            if (active && payload && payload.length) {
+                return (
+                    <div className="lineChart__tooltip">
+                        <p>{`${payload[0].value}min`}</p>
+                    </div>
+                );
+            }
+            return null;
+        };
+
         return (
 
-            <ResponsiveContainer width="100%" height="100%">
-                <LineChart
-                    width={500}
-                    height={300}
-                    data={this.props.averageSessions}
-                    margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="day" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Line type="monotone" dataKey="sessionLength" stroke="#8884d8" activeDot={{ r: 8 }} />
-                </LineChart>
-            </ResponsiveContainer>
+            <div className="lineChart">
+                <ResponsiveContainer width="100%" height="100%">
+                    <LineChart
+                        width={500}
+                        height={300}
+                        data={this.props.averageSessions}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+
+                        <XAxis
+                            dataKey="day"
+                            className="lineChart__legend"
+                            stroke="white" dataKey="day"
+                            axisLine={false}
+                            tickLine={false}
+                        />
+                        <Tooltip content={<CustomTooltip />}/>
+                        <Line
+                            type="natural"
+                            dataKey="sessionLength"
+                            stroke="#8884d8"
+                            activeDot={{ r: 8 }} />
+                    </LineChart>
+                </ResponsiveContainer>
+            </div>
+
         );
     }
 }
