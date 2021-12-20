3eb514a7cd5161791b2d9eb5e680387beda12d8b
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-12-20 09:39:09 +0100

    add RadarChart Component basic structure files

---
+++ src/Components/Charts/RadarChart/RadarChart.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./RadarChart.scss";
+
+class RadarChart extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+
+}
+
+export default RadarChart ;
